7082ba1622ae073b19a4efd9c0ccf9eec21f97f5
linlin <linlin@163.com>
2017-12-19 22:02:18 +0800

    搭建Spring Boot项目

---
+++ src/main/java/cn/lyhxh/LinLinApplication.java
@@ -0,0 +1,13 @@
+package cn.lyhxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinLinApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(LinLinApplication.class, args);
+    }
+
+}
